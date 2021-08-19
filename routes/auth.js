4db6fee5a78fc43a55dee2d9e539077a881b9598
judemin <judemin2087@naver.com>
2021-08-19 21:10:23 +0900

    add : login function, res req body

--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+  //로그인시 아이디와 비밀번호 json으로 넘겨줌
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) {
+      return res.json({
+        loginSuccess: false,
+        message: "Invalid Email",
+      });
+    }
+    user
+      .comparePassword(req.body.password)
+      .then((isMatch) => {
+        if (!isMatch) {
+          return res.json({
+            loginSuccess: false,
+            message: "Invalid Password",
+          });
+        }
+    //비밀번호 일치했을 때 토큰 생성
+        user
+          .generateToken() //jwt 토큰 생성
+          .then((user) => {
+            res
+              .cookie("x_auth", user.token)
+              .status(200)
+              .json({ loginSuccess: true, userId: user._id });
+          })
+          .catch((err) => {
+            res.status(400).send({loginSuccess: false, err});
+          });
+      })
+      .catch((err) => res.json({ loginSuccess: false, err }));
+
+  });
 });
 
 router.post('/register', function(req, res, next) {
   //post로 넘어온 데이터를 받아서 DB에 저장
   //user 모델에서 mongoose에 연결 => 바로 데이터베이스에 저장
   const user = new User(req.body);
+  // req의 body는 
 
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
