bfe528dade3cfa6529f10e942595e6cc6365e757
judemin <judemin2087@naver.com>
2021-08-29 21:47:00 +0900

    add : add bootpay token getter

--- routes/payment.js
+++ routes/payment.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const RestClient = require('@bootpay/server-rest-client').RestClient;
 
 router.get('/', function(req, res, next) {
     res.status(200).send("Bootpay API");
 });
 
 router.get('/verification', function(req, res, next) {
+    RestClient.setConfig(
+        "61176c377b5ba4001d52a085",
+        "pP3BMgNmcpS/ccwJECDzgVv6Lo6aOedyZmLpdnZzPZQ="
+    );
+    
+    RestClient.getAccessToken().then(function (response) {
+        let token = null;
 
+        if (response.status === 200) {
+            token = response.data.token;
+            console.log(token);
+
+            res.status(200).send("Bootpay Verification");
+        } else{
+            res.status(400).send("Failed to get BP Token");
+        }
+    });
 });
 
-
 module.exports = router;
+
+/*
+612b657c0199430036b50f7d
+
+612b65530d681b0039e5fad1
+*/
