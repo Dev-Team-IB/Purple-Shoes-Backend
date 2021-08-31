c91028ac507f8daea1ac4c017a881e791718be3a
judemin <judemin2087@naver.com>
2021-08-31 18:33:43 +0900

    add : add bootpay receipt info lookup

--- routes/payment.js
+++ routes/payment.js
@@ -7,29 +7,40 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/verification', function(req, res, next) {
+
+    let receipt_id = req.body.receipt_id
+
     RestClient.setConfig(
         "61176c377b5ba4001d52a085",
         "pP3BMgNmcpS/ccwJECDzgVv6Lo6aOedyZmLpdnZzPZQ="
     );
-    
+
     RestClient.getAccessToken().then(function (response) {
-        let token = null;
-
-        if (response.status === 200) {
-            token = response.data.token;
-            console.log(token);
-
-            res.status(200).send("Bootpay Verification");
-        } else{
+        // Access Token을 발급 받았을 때
+        if (response.status === 200 && response.data.token !== undefined) {
+            RestClient.verify(receipt_id).then(function (_response) {
+                // 검증 결과를 제대로 가져왔을 때
+                if (_response.status === 200) {
+                    console.log(_response);
+                    res.status(200).send(_response);
+                } else{
+                    res.status(400).send("Failed to get receipt info");
+                }
+            }).catch(
+                function (error){
+                    res.status(400).send(error);
+                    console.log(error);
+                }
+            );
+        }  else{
             res.status(400).send("Failed to get BP Token");
         }
     });
+    
 });
 
 module.exports = router;
 
-/*
-612b657c0199430036b50f7d
 
-612b65530d681b0039e5fad1
-*/
+// 612b657c0199430036b50f7d
+// 612b65530d681b0039e5fad1
